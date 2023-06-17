f0791b1764d41ff772d919f36b0aaffe81eba625
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-06-17 17:56:07 +0300

    v_CurrencyType

--- EasyCashIdentityProject.Entity/Concrete/CustomerAccountProcess.cs
+++ EasyCashIdentityProject.Entity/Concrete/CustomerAccountProcess.cs
@@ -16,5 +16,6 @@ namespace EasyCashIdentityProject.Entity.Concrete
         public int? ReceiverID { get; set; }
         public CustomerAccount SenderCustomer { get; set; }
         public CustomerAccount ReceiverCustomer { get; set; }
+        public string? Description { get; set; }
     }
 }
--- EasyCashIdentityProject.MvcWebUI/Controllers/SendMoneyController.cs
+++ EasyCashIdentityProject.MvcWebUI/Controllers/SendMoneyController.cs
@@ -19,8 +19,9 @@ namespace EasyCashIdentityProject.MvcWebUI.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string mycurrency)
         {
+            ViewBag.currency = mycurrency;    
             return View();
         }
         [HttpPost]
@@ -30,23 +31,20 @@ namespace EasyCashIdentityProject.MvcWebUI.Controllers
 
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             var receiverAccountNumber = context.CustomerAccounts.Where(x => x.CustomerAccountNumber == sendMoneyForCustomerAccountProcessDto.ReceiverAccountNumber).Select(y => y.CustomerAccountID).FirstOrDefault();
+            var senderAccountNumberID = context.CustomerAccounts.Where(z => z.AppUserID == user.Id).Where(y => y.CustomerAccountCurrency == "Türk Lirası").Select(x => x.CustomerAccountID).FirstOrDefault();
 
-            sendMoneyForCustomerAccountProcessDto.SenderID = user.Id;
-            sendMoneyForCustomerAccountProcessDto.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
-            sendMoneyForCustomerAccountProcessDto.ProcessType = "Havale";
-            sendMoneyForCustomerAccountProcessDto.ReceiverID = receiverAccountNumber;
 
             var values = new CustomerAccountProcess();
             values.ProcessDate= Convert.ToDateTime(DateTime.Now.ToShortDateString());
-            values.SenderID = 1;
+            values.SenderID = senderAccountNumberID;
             values.ProcessType = "Havale";
             values.ReceiverID = receiverAccountNumber;
             values.Amount=sendMoneyForCustomerAccountProcessDto.Amount;
-
+            values.Description= sendMoneyForCustomerAccountProcessDto.Description;
             _customerAccountProcessService.TInsert(values);
 
 
             return RedirectToAction("Index", "Deneme");
-        }
+        }    
     }
 }
